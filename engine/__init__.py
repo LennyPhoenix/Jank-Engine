bc89c70c03a5bb071de1a712f514b541d2d1da6b
Lenny P Critchley <doaltplusf4@gmail.com>
2020-06-11 20:20:52 +0100

    Added networking module.
    Empty Client and Server classes.

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,6 +1,6 @@
 from pyglet.window import key, mouse
 
-from . import collider_dicts
+from . import collider_dicts, networking
 from .application import Application
 from .camera import Camera
 from .entity import Entity
---
+++ engine/networking/server.py
@@ -0,0 +1,10 @@
+import pickle
+import socket
+import threading
+import time
+
+from engine.application import Application
+
+
+class Server(Application):
+    pass
