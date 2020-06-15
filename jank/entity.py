ebabd5c21e2495ba2669cec370f869b595cdd858
Lenny P Critchley <doaltplusf4@gmail.com>
2020-06-15 19:03:16 +0100

    Reworked entity system.

--- jank/entity.py
+++ jank/entity.py
@@ -7,19 +7,18 @@ import pymunk
 from .collider_dicts import dict_to_collider
 
 
-class Entity(pymunk.Body):
+class Entity:
     _space = None
-    _active = True
     _flip = False
 
     def __init__(
         self,
-        position=(0, 0), rotation=0,
-        body_type=pymunk.Body.DYNAMIC,
+        position: tuple = (0, 0), rotation: float = 0,
+        body_type: int = pymunk.Body.DYNAMIC,
         mass: float = 1, moment: float = float("inf"),
         colliders: list = None, collider: dict = None
     ):
-        super().__init__(mass=mass, moment=moment, body_type=body_type)
+        self.body = pymunk.Body(mass=mass, moment=moment, body_type=body_type)
         self.position = position
         self.angle = math.radians(rotation)
         self.colliders = []
@@ -31,43 +30,71 @@ class Entity(pymunk.Body):
             self.add_collider(collider)
 
     @property
-    def active(self):
-        return self._active
-
-    @active.setter
-    def active(self, active):
-        if active:
-            for collider in self.colliders:
-                collider.body = self
-        else:
-            for collider in self.colliders:
-                collider.body = None
-
-        self._active = active
-
-    @property
     def space(self):
         return self._space
 
     @space.setter
-    def space(self, space):
+    def space(self, space: pymunk.Space):
         if self.space is not None:
-            self.space.remove(self, *self.colliders)
+            self.space.remove(self.body, *self.colliders)
 
         self._space = space
         if space is not None:
-            self.space.add(self, *self.colliders)
+            self.space.add(self.body, *self.colliders)
 
-    def add_collider(self, collider):
+    @property
+    def position(self):
+        return self.body.position
+
+    @position.setter
+    def position(self, position: tuple):
+        self.body.position = position
+
+    @property
+    def angle(self):
+        return self.body.angle
+
+    @angle.setter
+    def angle(self, angle: float):
+        self.body.angle = angle
+
+    @property
+    def angle_degrees(self):
+        return math.degrees(self.angle)
+
+    @angle_degrees.setter
+    def angle_degrees(self, angle_degrees: float):
+        self.angle = math.radians(angle_degrees)
+
+    @property
+    def flip(self):
+        return self._flip
+
+    @flip.setter
+    def flip(self, flip: bool):
+        if self._flip != flip:
+            if flip:
+                self.sprite.scale_x = -(abs(self.sprite.scale_x))
+            else:
+                self.sprite.scale_x = abs(self.sprite.scale_x)
+            self._flip = flip
+            self.update_sprite()
+
+    def add_collider(self, collider: dict):
         col = dict_to_collider(collider)
-        col.body = self
+        col.body = self.body
 
         if self.space is not None:
             self.space.add(col)
 
         self.colliders.append(col)
 
-    def create_sprite(self, image, offset, batch=None, group=None):
+    def create_sprite(
+        self,
+        image, offset: tuple,
+        batch: pyglet.graphics.Batch = None,
+        group: pyglet.graphics.Group = None
+    ):
         self.sprite_offset = pymunk.Vec2d(offset)
         pos = self.sprite_offset.rotated(self.angle)+self.position
         self.sprite = pyglet.sprite.Sprite(
@@ -76,21 +103,7 @@ class Entity(pymunk.Body):
             batch=batch,
             group=group
         )
-        self.sprite.rotation = math.degrees(self.angle)
-
-    @property
-    def flip(self):
-        return self._flip
-
-    @flip.setter
-    def flip(self, flip):
-        if self._flip != flip:
-            if flip:
-                self.sprite.scale_x = -(abs(self.sprite.scale_x))
-            else:
-                self.sprite.scale_x = abs(self.sprite.scale_x)
-            self._flip = flip
-            self.update_sprite()
+        self.sprite.rotation = self.angle_degrees
 
     def update_sprite(self):
         pos = self.sprite_offset
@@ -103,17 +116,3 @@ class Entity(pymunk.Body):
     def delete(self):
         self.space = None
         self.sprite.delete()
-
-    def __getstate__(self):
-        d = super().__getstate__()
-
-        d["special"].append(("active", self._active))
-
-        return d
-
-    def __setstate__(self, state):
-        super().__setstate__(state)
-
-        for k, v in state["special"]:
-            if k == "active":
-                self._active = v
