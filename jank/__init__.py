97f0d86224999de96d87afe00bcef25bf512a5d8
Lenny P Critchley <doaltplusf4@gmail.com>
2020-06-19 17:07:57 +0100

    Added state machine class.

--- jank/__init__.py
+++ jank/__init__.py
@@ -5,3 +5,4 @@ from .application import Application
 from .camera import Camera
 from .entity import Entity
 from .load_animation import load_animation
+from .state_machine import StateMachine
---
+++ jank/state_machine.py
@@ -0,0 +1,22 @@
+import pyglet
+
+
+class StateMachine(pyglet.event.EventDispatcher):
+    _state = None
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, state):
+        if self.state != state:
+            previous_state = self.state
+            self._state = state
+            self.dispatch_event("on_state_change", state, previous_state)
+
+    def on_state_change(self, state, previous_state):
+        """ Called after state change. """
+
+
+StateMachine.register_event_type("on_state_change")
