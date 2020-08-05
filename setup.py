741503b2497f8790209c19dd1ac257b4e8c35475
Lenny P Critchley <doaltplusf4@gmail.com>
2020-08-05 16:17:19 +0100

    Added higher python requirement to setup.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.4',
+    python_requires='>=3.6',
     install_requires=[
         "pyglet>=1.5.7", "pymunk"
     ]
