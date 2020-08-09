02be329b67136e51f72c241fbdc1b58bea9ee506
Lenny P Critchley <doaltplusf4@gmail.com>
2020-08-09 19:45:20 +0100

    Added Rect class.

--- jank/ui/__init__.py
+++ jank/ui/__init__.py
@@ -1,7 +1,9 @@
 from .base import Base
+from .rectangle import Rect
 from .constants import BOTTOM, CENTER, LEFT, RIGHT, TOP
 
 __all__ = [
     "Base",
+    "Rect",
     "BOTTOM", "CENTER", "LEFT", "RIGHT", "TOP"
 ]
---
+++ jank/ui/rectangle.py
@@ -0,0 +1,46 @@
+import jank
+
+from .base import Base
+
+
+class Rect(Base):
+    _x: float
+    _y: float
+
+    def __init__(self, x, y, width, height, colour, parent=None):
+        self._x = x
+        self._y = y
+        self.rect = jank.shape_sprites.Rectangle(
+            x, y, width, height,
+            color=colour,
+            batch=jank.get_app().ui_batch
+        )
+        super().__init__(parent)
+
+    def update_sprite(self):
+        self.rect.x = self.real_x
+        self.rect.y = self.real_y
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
+
+    def get_width(self):
+        return self.rect.width
+
+    def get_height(self):
+        return self.rect.height
+
+    def set_x(self, x):
+        self._x = x
+
+    def set_y(self, y):
+        self._y = y
+
+    def set_width(self, width):
+        self.rect.width = width
+
+    def set_height(self, height):
+        self.rect.height = height
