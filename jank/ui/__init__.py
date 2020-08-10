e142e252d4eedbf421cd956447f264933874d5c4
Lenny P Critchley <doaltplusf4@gmail.com>
2020-08-10 10:07:17 +0100

    Added Text class.

--- jank/ui/__init__.py
+++ jank/ui/__init__.py
@@ -3,6 +3,7 @@ from .circle import Circle
 from .constants import BOTTOM, CENTER, LEFT, RIGHT, TOP
 from .rectangle import Rect
 from .sprite import Sprite
+from .text import Text
 
 __all__ = [
     "Base",
@@ -10,4 +11,5 @@ __all__ = [
     "BOTTOM", "CENTER", "LEFT", "RIGHT", "TOP",
     "Rect",
     "Sprite",
+    "Text",
 ]
